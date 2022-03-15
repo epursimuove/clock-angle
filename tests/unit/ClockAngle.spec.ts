0613ea8c4d70f86e3718397770d017d251ba36b3
Anders Gustafson <github@nemonisimors.com>
2022-03-15 12:06:56 +0100

    Updated versions.

--- tests/unit/ClockAngle.spec.ts
+++ tests/unit/ClockAngle.spec.ts
@@ -11,4 +11,26 @@ describe("ClockAngle.vue", () => {
         expect(wrapper.html()).toContain("Showing digital clock");
         expect(wrapper.html()).toContain("canvas");
     });
+
+    // describe("Calculate state", () => {
+    //
+    //     beforeAll(() => {
+    //         jest.useFakeTimers("modern");
+    //         jest.setSystemTime(new Date(2021, 11, 24, 15, 20, 45));
+    //     });
+    //
+    //     afterAll(() => {
+    //         jest.useRealTimers();
+    //     });
+    //
+    //     it("calculates state correctly", () => {
+    //
+    //         const wrapper = mount(ClockAngle);
+    //
+    //
+    //         expect(wrapper.vm.state.secondHand.angleDeg).toBe(false);
+    //
+    //
+    //     });
+    // });
 });
